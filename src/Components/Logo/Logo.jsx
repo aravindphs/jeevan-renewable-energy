d614b6bb767d628ff3bc8e202105de38baf51fa2
Aravind Kumar J <aravind94977034@gmail.com>
2025-05-01 17:16:33 +0530

    added and styled logo

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import "./Logo.css";
 import { PiSolarPanelFill } from "react-icons/pi";
+import LogoJRE from "./logologo.png";
 
 const Logo = () => {
   return (
     <div className="logo">
-      <PiSolarPanelFill className="icon" />
+      {/* <PiSolarPanelFill className="icon" />
       <h1 className="name">
         JR<span className="color__primary">Energy</span>
-      </h1>
+      </h1> */}
+      <img src={LogoJRE} className="icon" alt="" />
     </div>
   );
 };
